976cc8236ffa1f012f486408c29aadfa92367f55
MD <mdoremy@gmail.com>
2022-05-10 08:58:24 -0400

    Fixed App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,52 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
-import { BrowserRouter, Route } from  'react-router-dom';
+import TaskList from './TaskList';
+import AddTask from './AddTask';
 
-const
+class App extends React.Component {
+    state = {
+        tasks: [],
+        errorMessage: ''
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
+            .then(response => {
+                this.setState({ tasks: response.data });
+            }).catch(error => {
+            this.setState({ errorMessage: error.message });
+        });
+    }
+
+    onAddTask = (taskName) => {
+        let tasks = this.state.tasks;
+        tasks.push({
+            title: taskName,
+            id: this.state.tasks.length + 1,
+            type: 'task',
+            column: 'todo'
+        });
+
+        this.setState({ tasks });
+    }
+
+    onUpdateTaskList = (newTaskList) => {
+        this.setState({ tasks: newTaskList });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <AddTask onSubmit={this.onAddTask} />
+                <TaskList tasks={this.state.tasks} onUpdateTaskList={this.onUpdateTaskList} />
+            </div>
+        );
+    }
+}
+
+export default App;
